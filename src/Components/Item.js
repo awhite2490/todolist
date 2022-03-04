42e2097e60bcfdf9ce02d645fdaa9298a2c5d9fc
Andrew White <42251098+awhite2490@users.noreply.github.com>
2022-03-04 16:50:36 -0500

    Adding styling to completed items

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Item = (props) => {
     const [isComplete, setIsComplete] = React.useState(props.item.isComplete)
+    const [className, setClassName] = React.useState('');
 
     const removeItem = () => {
         props.removeItem(props.item)
@@ -10,10 +11,15 @@ const Item = (props) => {
     const handleCheck = () => {
         setIsComplete(!isComplete);
         props.toggleItemComplete(props.item);
+        if(!isComplete) {
+            setClassName('Item-Complete')
+        } else {
+            setClassName('')
+        }
     }
 
     return (   
-        <li>
+        <li className = {className}>
             {props.item.text}
             <input 
                 type="checkbox" 
